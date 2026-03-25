f64659d73e03b5f3d5f4e4bcc1b768c2b77dca72
Nazarii-Lesniak <naziksay@gmail.com>
2026-03-25 17:35:33 +0200

    feat: initialize prisma schema and synchronize postgres database

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-'use strict';
+import 'dotenv/config';
+import express from 'express';
+import cookieParser from 'cookie-parser';
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.get('/', (request, response) => {
+  response.send('Server is runnig;');
+});
+
+const PORT = process.env.PORT || 3005;
+
+app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
+  console.log(
+    `\x1b[32m[READY]\x1b[0m Server is running on \x1b[36mhttp://localhost:${PORT}\x1b[0m`,
+  );
+});
