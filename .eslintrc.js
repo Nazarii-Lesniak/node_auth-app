9a978ec112e94282ff7da872881ffd21c24f71ba
Nazarii-Lesniak <naziksay@gmail.com>
2026-03-28 10:33:13 +0200

    fix: update projectType to typescript and fix ESLint overrides

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,7 +10,7 @@ module.exports = {
   plugins: ['jest'],
   overrides: [
     {
-      files: ['*.ts', '*.tsx'],
+      files: ['**/*.ts', '**/*.tsx'],
       parser: '@typescript-eslint/parser',
       plugins: ['@typescript-eslint'],
       extends: ['plugin:@typescript-eslint/recommended'],
